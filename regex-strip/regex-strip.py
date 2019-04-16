562284298ecfdf15dda66463bf7def57b028a179
kudeh <kene.udeh@yahoo.com>
2019-04-16 19:51:31 -0400

    ch 7 folders & files added

---
+++ regex-strip/regex-strip.py
@@ -0,0 +1,16 @@
+# regex-strip.py
+# Author: Kene Udeh
+# Source: Automate the Boring stuff with python Ch. 7 Project
+
+def strip(text):
+    """ python's str.strip() method implemented using regex
+    Args:
+        text (str): text to strip of white space
+    Returns:
+        stripped (str): text stripped of white space
+    """
+    pass
+
+if __name__ == "__main__":
+    text = ' test ffs   '
+    strip(text)
---
+++ strong-password-detector/strong-password.py
@@ -0,0 +1,16 @@
+# strong-password.py
+# Author: Kene Udeh
+# Source: Automate the Boring stuff with python Ch. 7 Project
+
+def testPasswordStrength(password):
+    """ check for at least eight characters long, contains both uppercase and lowercase characters, and has at least one digit
+    Args:
+        password (str): passoword as string
+    Returns:
+        strong (bool): True if password is strong else
+    """
+    pass
+
+if __name__ == "__main__":
+    password = 'Afwfei4f'
+    testPasswordStrength(password)
