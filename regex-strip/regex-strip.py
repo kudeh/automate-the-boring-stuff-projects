08d0fc5a62cb1ad594343b431694d09a1825abb4
kudeh <kene.udeh@yahoo.com>
2019-04-17 18:40:45 -0400

    completed regex strip project

--- regex-strip/regex-strip.py
+++ regex-strip/regex-strip.py
@@ -2,15 +2,23 @@
 # Author: Kene Udeh
 # Source: Automate the Boring stuff with python Ch. 7 Project
 
+import re
+
 def strip(text):
     """ python's str.strip() method implemented using regex
     Args:
         text (str): text to strip of white space
     Returns:
-        stripped (str): text stripped of white space
+        textStripped (str): text stripped of white space
     """
-    pass
+    stripStartRegex = re.compile(r'(^\s*)')
+    stripEndRegex = re.compile(r'(\s*$)')
+
+    textStartStripped = stripStartRegex.sub('', text)
+    textStripped = stripEndRegex.sub('', textStartStripped)
+
+    return textStripped
 
 if __name__ == "__main__":
     text = ' test ffs   '
-    strip(text)
+    print(strip(text))
