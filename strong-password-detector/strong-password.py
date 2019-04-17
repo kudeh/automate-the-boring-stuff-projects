8b849c61894cf985ccf7a4d08f6ca328c58d6e4d
kudeh <kene.udeh@yahoo.com>
2019-04-17 14:53:08 -0400

    completed password strength check project

--- strong-password-detector/strong-password.py
+++ strong-password-detector/strong-password.py
@@ -2,15 +2,33 @@
 # Author: Kene Udeh
 # Source: Automate the Boring stuff with python Ch. 7 Project
 
+import re
+
 def testPasswordStrength(password):
     """ check for at least eight characters long, contains both uppercase and lowercase characters, and has at least one digit
     Args:
-        password (str): passoword as string
+        password (str): password as string
     Returns:
         strong (bool): True if password is strong else
     """
-    pass
+
+    eightCharsLongRegex = re.compile(r'[\w\d\s\W\D\S]{8,}')
+    upperCaseRegex = re.compile(r'[A-Z]+')
+    lowerCaseRegex = re.compile(r'[a-z]+')
+    oneOrMoreDigitRegex = re.compile(r'\d+')
+    
+    if not eightCharsLongRegex.search(password):
+        return False
+    elif not upperCaseRegex.search(password):
+        return False
+    elif not lowerCaseRegex.search(password):
+        return False
+    elif not oneOrMoreDigitRegex.search(password):
+        return False
+
+    return True
+    
 
 if __name__ == "__main__":
-    password = 'Afwfei4f'
-    testPasswordStrength(password)
+    password = 'A&dsas9$_'
+    print(testPasswordStrength(password))
