46ff253d58664ba283e1fc19186722b5601216ab
kudeh <kene.udeh@yahoo.com>
2019-04-21 20:31:29 -0400

    madlibs project completed

--- mad-libs/mad-libs.py
+++ mad-libs/mad-libs.py
@@ -3,18 +3,41 @@
 # Source: Automate the Boring stuff with python Ch. 8 Project
 
 import os
+import re
 
-def madLibs(filename):
+def madLibs(input_file, output_file):
     """lets the user add their own text anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file
     Args:
         filename (str): name of file to parse
     Returns:
         None
     """
-    #with open(filename) as file:
+    regex = re.compile(r'\w*(NOUN|ADJECTIVE|ADVERB|VERB)\w*')
 
-    #    for line in file:
+    with open(input_file, 'r') as in_file, open(output_file, 'w') as out_file:
+
+        words = in_file.read().split(' ')
+        result = []
+
+        for w in words:
+
+            mo = regex.search(w)
+
+            if mo:
+                match = mo.group(1)
+                print(f'Enter a {match.lower()}: ', end='')
+                i = input()
+                result.append(regex.sub(i, w))
+            else:
+                result.append(w)
+   
+
+        out_file.write(' '.join(result))
+
+                
+
+
             
 
 if __name__ == "__main__":
-    pass
+    madLibs('input.txt', 'output.txt')
