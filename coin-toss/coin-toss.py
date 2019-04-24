f9d747a9a89a79eaa47588565fe73050f895d241
kudeh <kene.udeh@yahoo.com>
2019-04-24 09:20:30 -0400

    completed coin toss project

---
+++ coin-toss/coin-toss.py
@@ -0,0 +1,27 @@
+#! python3
+# coin-toss.py
+# Author: Kene Udeh
+# Source: Automate the Boring stuff with python Ch. 10 Project
+
+import random
+
+if __name__ == "__main__":
+
+    guess = ''
+    options = ['tails', 'heads']
+
+    while guess not in ('heads', 'tails'):
+        print('Guess the coin toss! Enter heads or tails:')
+        guess = input()
+
+    toss = random.randint(0, 1) # 0 is tails, 1 is heads
+
+    if guess == options[toss]:
+        print('You got it!')
+    else:
+        print('Nope! Guess again!')
+        guess = input()
+        if guess == options[toss]:
+            print('You got it!')
+        else:
+            print('Nope. You are really bad at this game.')
