5a7fc53b8cc8e55a44cabdc897b04d4ad07d0ac8
kudeh <kene.udeh@yahoo.com>
2019-04-26 22:39:56 -0400

    init commit for commandline emailer

---
+++ command-line-email/command-line-emailer.py
@@ -0,0 +1,14 @@
+#! python3
+# command-line-emailer.py
+# Author: Kene Udeh
+# Source: Automate the Boring stuff with python Ch. 11 Project
+
+def email():
+    """
+
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
