9e6cecd6079cbb052b34bad6cb9ddcf48f834b4c
Ahmed <ahmed.com@protonmail.com>
2019-06-30 02:05:49 +0200

    Update collatz.py

--- the-collatz-sequence/collatz.py
+++ the-collatz-sequence/collatz.py
@@ -13,16 +13,15 @@ def collatz(number):
     if (number % 2) == 0:
         print(number // 2)
         return number // 2
-    
-    print(3 * number + 1)
-    return 3 * number + 1
+    elif number % 2 == 1:
+        print(3 * number + 1)
+        return 3 * number + 1
 
 
-if __name__ == '__main__':
-    
+"""if __name__ == '__main__':
+"""   
 
     try:
-        
         num = int(input('Enter a number: '))
         result = collatz(num)
         while(result != 1):
